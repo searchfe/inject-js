3057c6692f47056bb05a02874cfb3bc6895117da
chaiyanchen <chaiyanchen@baidu.com>
2020-09-11 19:12:22 +0800

    feat: 依赖注入模块精简

--- test/di/value-provider-impl.spec.ts
+++ test/di/value-provider-impl.spec.ts
@@ -1,10 +1,10 @@
-import { createValueProvider } from '../../src/di/value-provider-impl'
+import { createValueProvider } from '../../src/di/value-provider-impl';
 
 describe('ValueProviderImpl', () => {
-  describe('#create', () => {
-    it('直接返回固定对象', () => {
-      const P = createValueProvider('foo')
-      expect(new P().create()).toEqual('foo')
-    })
-  })
-})
+    describe('#create', () => {
+        it('直接返回固定对象', () => {
+            const P = createValueProvider('foo');
+            expect(new P().create()).toEqual('foo');
+        });
+    });
+});

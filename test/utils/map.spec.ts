53253a06af5e058de741ec8ae310751a55f3b54a
harttle <yangjvn@126.com>
2020-11-02 19:28:34 +0800

    style: lint test/

--- test/utils/map.spec.ts
+++ test/utils/map.spec.ts
@@ -1,15 +1,15 @@
-import { Map } from '../../src/utils/map'
+import { Map } from '../../src/utils/map';
 
 describe('Map', () => {
-  it('支持存取', () => {
-    const map = new Map();
-    map.set('foo', 'bar');
-    expect(map.get('foo')).toEqual('bar');
-  })
-  it('支持清空', () => {
-    const map = new Map();
-    map.set('foo', 'bar');
-    map.clear();
-    expect(map.get('foo')).toEqual(null);
-  })
-})
+    it('支持存取', () => {
+        const map = new Map();
+        map.set('foo', 'bar');
+        expect(map.get('foo')).toEqual('bar');
+    });
+    it('支持清空', () => {
+        const map = new Map();
+        map.set('foo', 'bar');
+        map.clear();
+        expect(map.get('foo')).toEqual(null);
+    });
+});

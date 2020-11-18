525ff04c6d4424eaf8a7f5bac7e0e9e5732d6cba
chaiyanchen <chaiyanchen@baidu.com>
2020-11-18 17:26:40 +0800

    fix: 补充token类型产出

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Container } from './di/container';
 import { injectable } from './decorators/injectable';
 import { inject } from './decorators/inject';
 import { setDependencies, getDependencies, setNthDependency } from './di/dependency';
-import { createInjectToken } from './di/inject-token';
+import { createInjectToken, InjectToken } from './di/inject-token';
 
 export {
     Container,
@@ -11,5 +11,6 @@ export {
     setDependencies,
     getDependencies,
     setNthDependency,
-    createInjectToken
+    createInjectToken,
+    InjectToken
 };
